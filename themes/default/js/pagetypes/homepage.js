69ebd07377beaa03f757bf5bb374b13fab0d74f7
Simon Winter <simon@simonwinter.co.nz>
2013-10-23 22:40:41 +1300

    Team page updates

--- themes/default/js/pagetypes/homepage.js
+++ themes/default/js/pagetypes/homepage.js
@@ -24,7 +24,7 @@ require(['jquery', 'backbone', 'underscore', '_base'], function($, Backbone, _) 
 	
 	$(function() {
 		$(window).resize(function() {
-			$('#work .block').height($(window).height() - $('#header').height());
+			$('#work .block').height($(window).height() - $('#logo').height());
 			
 			
 			$('body:not(.mobile) .contain').each(function() {
