e52ca844e4f3d97e546303010e079feaa8b58b31
Simon Winter <simon@saltedherring.com>
2013-11-25 09:13:35 +1300

    Updates to about page

--- themes/default/js/pagetypes/aboutpage.js
+++ themes/default/js/pagetypes/aboutpage.js
@@ -2,7 +2,14 @@
 require(['jquery', 'backbone', 'underscore', '_base'], function($, Backbone, _) {
 	
 	$(function() {
-		
+		$(window).scroll(function() {
+			var offset = $(window).scrollTop();
+			
+			$('.overlay').css({
+				top: 0 - (offset / 20) 
+			});
+			
+		}).scroll();
 	});
 	
 });
