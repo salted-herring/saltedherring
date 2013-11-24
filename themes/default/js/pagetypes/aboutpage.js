4584208dbda617e1aaf2fd816148cadf3b0f6051
Simon Winter <simon@saltedherring.com>
2013-11-25 09:17:31 +1300

    Updates to about page.

--- themes/default/js/pagetypes/aboutpage.js
+++ themes/default/js/pagetypes/aboutpage.js
@@ -6,7 +6,7 @@ require(['jquery', 'backbone', 'underscore', '_base'], function($, Backbone, _) 
 			var offset = $(window).scrollTop();
 			
 			$('.overlay').css({
-				top: 0 - (offset / 20) 
+				top: 0 - (offset / 17) 
 			});
 			
 		}).scroll();
