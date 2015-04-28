8099b42e2f379f51070842bcddfe47040c7c50ad
Simon Winter <simon@saltedherring.com>
2015-04-29 11:37:58 +1200

    patch applied to seo extension

--- mainsite/code/Model/Project.php
+++ mainsite/code/Model/Project.php
@@ -9,35 +9,35 @@ class Project extends BaseDBO {
 		'SiteURL' => 'Varchar(255)',
 		'isPublished' => 'Boolean'
 	);
-	
+
 	private static $has_one = array(
 		'Client' => 'Client',
 		'Colour' => 'Colour'
 	);
-	
+
 	private static $has_many = array(
 		'ProjectAwards' => 'ProjectAward',
 		'Media' => 'BaseMedia'
 	);
-	
+
 	private static $many_many = array(
 		'Categories' => 'Category',
 		'Services' => 'Service',
 		'RelatedProjects' => 'Project'
 	);
-	
+
 	private static $belongs_to = array(
 		'Slider' => 'Slider'
 	);
-	
+
 	private static $belongs_many_many = array(
 		'Project' => 'Project'
 	);
-	
+
 	private static $defaults = array(
 		'isPublished' => true
 	);
-	
+
 	private static $summary_fields = array(
 		'Title',
 		'isPublished'
@@ -45,7 +45,7 @@ class Project extends BaseDBO {
 
 	public function getCMSFields() {
 		$fields = parent::getCMSFields();
-		
+
 		$fields->removeByName('Categories');
 		$fields->removeByName('ClientID');
 		$fields->removeByName('Services');
@@ -53,7 +53,7 @@ class Project extends BaseDBO {
 		$fields->removeByName('ProjectAwards');
 		$fields->removeByName('Project');
 		$fields->removeByName('RelatedProjects');
-		
+
 		$gridFieldConfig = GridFieldConfig::create()->addComponents(
 			new GridFieldToolbarHeader(),
 			new GridFieldSortableHeader(),
@@ -64,33 +64,33 @@ class Project extends BaseDBO {
 			new GridFieldDetailForm(),
 			new GridFieldFilterHeader()
 		);
-		
+
 		$add->setButtonName('Add Award');
-		
+
 		$cats = array();
-		
+
 		foreach(Category::get() as $cat) {
 			$cats[$cat->ID] = $cat->Name;
 		}
-		
+
 		$clients = array();
-		
+
 		foreach(Client::get() as $client) {
 			$clients[$client->ID] = $client->Name;
 		}
-		
+
 		$services = array();
-		
+
 		foreach(Service::get() as $service) {
 			$services[$service->ID] = $service->Name;
 		}
-		
+
 		$rel = array();
-		
+
 		foreach(Project::get()->exclude(array('ID' => $this->ID)) as $project) {
 			$rel[$project->ID] = $project->Title;
 		}
-		
+
 		// Root.Main
 		$fields->addFieldsToTab('Root.Main', array(
 			$title = new TextField('Title'),
@@ -102,30 +102,30 @@ class Project extends BaseDBO {
 			$client = new DropDownField('ClientID', 'Client', $clients),
 			$related = $this->ID ? new ListboxField($name = "RelatedProjects", $title = "Related Projects")  : new LiteralField('RelatedWarn', '<p><strong>Related Projects</strong> can be added once the project has been created</p>'),
 		));
-		
+
 		if($this->ID) {
 			$categories->setSource($cats);
 			$categories->setMultiple(true);
-			
+
 			$serv->setSource($services);
 			$serv->setMultiple(true);
-			
+
 			$related->setSource($rel);
 			$related->setMultiple(true);
 		}
-		
+
 		$tagLine->setRightTitle('Short, single line description of project');
 		$cite->setRightTitle('Quotation source. Can contain html tags - e.g. to link to twitter etc.');
 		$quote->setTitle('Quote - use “ & ” for quote marks.');
-		
+
 		if($this->ID) {
 			// Root.Award
 			$fields->addFieldToTab('Root.Awards', new GridField("Awards", "Awards", $this->ProjectAwards(), $gridFieldConfig));
-			
+
 			// Root.Media
 			$multiClasses = new GridFieldAddNewMultiClass();
 			$multiClasses->setClasses(array('ImageMedia', 'SWFMedia', 'VimeoMedia'));
-			
+
 			$gridFieldConfig = GridFieldConfig::create()->addComponents(
 				new GridFieldToolbarHeader(),
 				new GridFieldSortableHeader(),
@@ -137,31 +137,31 @@ class Project extends BaseDBO {
 				new GridFieldOrderableRows('SortOrder'),
 				$multiClasses
 			);
-			
+
 			$fields->addFieldToTab('Root.Media', new GridField("Media", "Media", $this->Media(), $gridFieldConfig));
-			
+
 			$url = new HiddenField('URLSegment');
 			$url->setAttribute('data-prefix', 'http://' . $_SERVER['HTTP_HOST']);
 			$url->setAttribute('value', $this->Link());
 			$fields->addFieldToTab('Root.Main', $url);
 		}
-		
+
 		return $fields;
 	}
-	
+
 
 	public function getURL() {
 		return '/work/project/' . $this->URLSegment;
 	}
-	
+
 	public function AbsoluteLink() {
 		return Director::absoluteURL($this->Link());
 	}
-	
+
 	public function Link() {
 		return $this->getURL();
 	}
-	
+
 	public function getFirstImage() {
 		$im = NULL;
 		foreach($this->Media() as $media) {
@@ -169,41 +169,58 @@ class Project extends BaseDBO {
 				return $media;
 			}
 		}
-		
+
 		return $im;
 	}
-	
+
 	public function getAllMedia() {
 		$curr = $this->getFirstImage();
-		
+
 		if($curr) {
 			return $this->Media()->exclude(array(
 				'ID' => $curr->ID
 			));
 		}
-		
+
 		return $this->Media;
 	}
-	
+
+	private function checkPageSubjectInFirstParagraph() {
+		if ($this->checkPageSubjectDefined()) {
+			$first_paragraph = $this->ProjectInfo->FirstParagraph();
+
+			if (trim($first_paragraph != '')) {
+				if (preg_match('/' . $this->SEOPageSubject . '/i', $first_paragraph)) {
+					return true;
+				}
+				else {
+					return false;
+				}
+			}
+		}
+
+		return false;
+	}
+
 	public function onBeforeWrite() {
 		parent::onBeforeWrite();
-		
+
 		if($this->MetaDescription == NULL) {
 			$this->MetaDescription = $this->Title . ' - ' . $this->TagLine;
 		}
-		
+
 		$dir = ROOT . 'themes/' . SiteConfig::current_site_config()->Theme . '/json/';
 		$data = array();
-		
+
 		foreach(Category::get() as $cat) {
 			$projects = Project::get()->filter(array('isPublished' => 1))->leftJoin('Project_Categories', 'Project.ID = Project_Categories.ProjectID')->filter('CategoryID', $cat->ID);
-			
+
 			$current = array(
 				'Category' => $cat->Title,
 				'URLSegment' => $cat->URLSegment,
 				'Projects' => array()
 			);
-			
+
 			foreach($projects as $project) {
 				array_push($current['Projects'], array(
 					'Title' => $project->Title,
@@ -211,7 +228,7 @@ class Project extends BaseDBO {
 					'URLSegment' => $project->URLSegment
 				));
 			}
-			
+
 			try {
 				$handle = fopen($dir . $cat->URLSegment . '.json', 'w');
 				fwrite($handle, json_encode($current));
@@ -220,9 +237,9 @@ class Project extends BaseDBO {
 				user_error($e, E_USER_WARNING);
 			}
 		}
-		
+
 		$all = array();
-		
+
 		foreach(Project::get()->filter(array('isPublished' => 1)) as $project) {
 			array_push($all, array(
 				'Title' => $project->Title,
@@ -230,7 +247,7 @@ class Project extends BaseDBO {
 				'URLSegment' => $project->URLSegment
 			));
 		}
-		
+
 		try {
 			$handle = fopen($dir . 'all.json', 'w');
 			fwrite($handle, json_encode($all));
@@ -239,18 +256,18 @@ class Project extends BaseDBO {
 			user_error($e, E_USER_WARNING);
 		}
 	}
-	
+
 	public function getValidRelatedProjects() {
 		return $this->RelatedProjects()->filter(array('isPublished' => 1));
 	}
-	
+
 	public function canView($member = null) {
 		if(Permission::check('ADMIN')) {
 			return true;
 		}
 		return $this->isPublished;
 	}
-	
+
 	public function getSiteConfig() {
 		return SiteConfig::current_site_config();
 	}
@@ -261,48 +278,48 @@ class ProjectAward extends DataObject {
 	private static $db = array(
 		'URL' => 'Varchar(255)'
 	);
-	
+
 	private static $has_one = array(
 		'Project' => 'Project',
 		'Award' => 'Award'
 	);
-	
+
 	private static $has_many = array(
 	);
-	
+
 	static $summary_fields = array(
 		'Award.Name' => 'Name',
 		'gettheURL' => 'URL'
 	);
-	
+
 	private static $defaults = array(
 	);
-	
+
 	static $searchable_fields = array(
 	);
-	
+
 	public function gettheURL() {
 		return DBField::create_field('HTMLVarchar', '<a href="' . $this->URL . '" target="_blank">' . $this->URL . '</a>');
 	}
-	
+
 	public function getCMSFields() {
 		$fields = parent::getCMSFields();
 		$fields->removeByName('ProjectID');
-		
+
 		$fields->addFieldsToTab('Root.Main', array(
 			$url = new TextField('URL')
 		));
-		
+
 		$url->setRightTitle('This can be the awards site - or even better the link to the project on the awards site.');
-		
+
 		return $fields;
 	}
-	
+
 	public function getTitle() {
 		if($this->Award()) {
 			return $this->Award()->Name;
 		}
-		
+
 		return $this->ID;
 	}
 }
