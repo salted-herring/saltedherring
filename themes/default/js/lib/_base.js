4e32cec758dcb7ea61fff7b8709ebbf030b1fb9d
Bryn Heveldt <bryn@saltedherring.co.nz>
2015-05-26 17:59:10 +1200

    quick push [deploy:development]

--- themes/default/js/lib/_base.js
+++ themes/default/js/lib/_base.js
@@ -9,32 +9,33 @@
 
 define(['jquery', 'backbone', 'router'], function($, Backbone, Router) {
 
-	
+
 	/* ===========================
 	 * Cross browser compatible docheight
 	 * =========================== */
 	window.docHeight = function() {
 		var body = document.body,
     		html = document.documentElement;
-    	
-		return Math.max( body.scrollHeight, body.offsetHeight, 
+
+		return Math.max( body.scrollHeight, body.offsetHeight,
                        html.clientHeight, html.scrollHeight, html.offsetHeight );
     }
-		
+
 	// ============================
 	// Show / hide the mobile menu.
 	//============================
 	$('#menu_icon').click(function(e) {
-		e.preventDefault();
+		// e.preventDefault();
+		alert();
 		$(this).toggleClass('collapse expand');
 		$('#main_nav').toggleClass('collapse expand');
 	});
-	
+
 	$(document).on('click', '#main_nav a, #logo a', function() {
 		$('#main_nav a, #logo a').removeClass('current');
 		$(this).addClass('current');
 	});
-	
+
 	$('#projectnav a').hover(function() {
 		var _w = $(this).outerWidth(true) + Math.max($(this).find('strong').outerWidth(), $(this).find('em').outerWidth());
 		if($(this).is('.next')) {
@@ -46,8 +47,8 @@ define(['jquery', 'backbone', 'router'], function($, Backbone, Router) {
 	}, function() {
 		$(this).removeClass('over').removeAttr('style');
 	});
-	
-	
+
+
 	$(document).on('hover', '#projectnav a', function() {
 		$(this).width($(this).outerWidth(true) + Math.max($(this).find('strong').outerWidth(), $(this).find('em').outerWidth()));
 		$(this).addClass('over');
@@ -55,15 +56,15 @@ define(['jquery', 'backbone', 'router'], function($, Backbone, Router) {
 		$(this).removeAttr('style');
 		$(this).removeClass('over');
 	});
-	
-	
+
+
 	$(window).scroll(function(e) {
 			if($(window).scrollTop() + $(window).height() >= $(document).height()) {
 				$('footer .salt').addClass('animate');
 			} else {
 				$('footer .salt').removeClass('animate');
 			}
-			
+
 			if($('body:not(.mobile) header.overlay:not(.no-images)').length > 0) {
 				/* ===========================
 				 * Animation for overlay as seen
@@ -71,7 +72,7 @@ define(['jquery', 'backbone', 'router'], function($, Backbone, Router) {
 				 * =========================== */
 				var headerHeight = $('header.overlay').outerHeight(true),
 					offset = 30;
-				
+
 				if($(window).scrollTop() < (180)) {
 					$('.detailscontainer:not(.nofix)').addClass('fixedcontainer');
 					$('.detailscontainer:not(.nofix)').css('margin-top', 0).parent().height($('.detailscontainer:not(.nofix)').height());
@@ -79,24 +80,24 @@ define(['jquery', 'backbone', 'router'], function($, Backbone, Router) {
 					if($('.detailscontainer:not(.nofix)').is('.fixedcontainer')) {
 						$('.detailscontainer:not(.nofix)').css('margin-top', 216);
 					}
-				
+
 					$('.detailscontainer:not(.nofix)').removeClass('fixedcontainer');
 				}
-				
+
 				if($(window).scrollTop() > 157) {
 					$('.overlay').addClass('small');
 				} else {
 					$('.overlay').removeClass('small').css('top',0);
 				}
 			}
-			
-			
-			
+
+
+
 			/* ===========================
 			 * Animation for banner as seen on
 			 * work & team landing pages.
 			 * =========================== */
-			
+
 			if($('body:not(.mobile) nav#banner').length > 0) {
 				if($(window).scrollTop() > 200) {
 					$('nav#banner').addClass('small');
@@ -110,10 +111,10 @@ define(['jquery', 'backbone', 'router'], function($, Backbone, Router) {
 					$('header').removeClass('delay').removeClass('small');//.css('top',0);
 				}
 			}
-			
-			
+
+
 			/* ===========================
-			 * Hide navigation at bottom 
+			 * Hide navigation at bottom
 			 * of the page.
 			 * =========================== */
 			if($('#projectnav').length > 0) {
@@ -121,16 +122,16 @@ define(['jquery', 'backbone', 'router'], function($, Backbone, Router) {
 					contentbottom = parseInt($('#content').css('padding-bottom')),
 					top = bottom-contentbottom,
 					current = $(window).scrollTop()+$(window).height();
-				
+
 				if(current >= top) {
 					$('#projectnav .next').css({
 						right: 0 - (current-top)
 					});
-					
+
 					$('#projectnav .previous').css({
 						left: 0 - (current-top)
 					});
-					
+
 					$('.overlay').css({
 						top: 102 - (current-top)
 					});
@@ -138,11 +139,11 @@ define(['jquery', 'backbone', 'router'], function($, Backbone, Router) {
 					$('#projectnav .next').css({
 						right: 0
 					});
-					
+
 					$('#projectnav .previous').css({
 						left: 0
 					});
-					
+
 					$('.overlay').css({
 						top: 102
 					});
