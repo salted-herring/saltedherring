f7504d4c84d81d23eada015b6290337a83cc2cb7
Bryn Heveldt <bryn@saltedherring.co.nz>
2015-05-27 15:15:18 +1200

    inital about section setup

--- mainsite/code/Layout/AboutPage.php
+++ mainsite/code/Layout/AboutPage.php
@@ -1,34 +1,42 @@
 <?php
 class AboutPage extends Page {
-	
+
 	private static $db = array(
 		'IntroText' => 'Text'
 	);
-	
+
 	private static $has_one = array(
 	);
-	
+
 	private static $has_many = array(
 	);
-	
+
 	public function getCMSFields() {
 		$fields = parent::getCMSFields();
-		
+
 		$fields->AddFieldToTab('Root.Main', new TextareaField('IntroText'));
 		$fields->AddFieldToTab('Root.Main', new HTMLEditorField('Content'));
-		
+
 		return $fields;
 	}
-	
+
 }
 class AboutPage_Controller extends Page_Controller {
-	
+
 	private static $allowed_actions = array (
+		'subsections'
 	);
-	
+
+	private static $url_handlers = array (
+		'$subsections!/$arg'	=> 'subsections'
+	);
+
+
 	public function init() {
 		parent::init();
-		
+
 	}
-	
+	public function subsections($request) {
+		return $this->renderWith(array('AboutSectionPage', 'Page'));
+	}
 }
