08c0f023f6dcaae63f2f770251361662978ef57b
Bryn Heveldt <bryn@saltedherring.co.nz>
2015-06-03 17:10:02 +1200

    new homepage and about and contact pages

--- themes/default/build/components/doc-ready/doc-ready.js
+++ themes/default/build/components/doc-ready/doc-ready.js
@@ -3,4 +3,4 @@
  * Cross browser DOMContentLoaded event emitter
  */
 
-(function(e){function t(e){if(typeof e!="function")return;t.isReady?e():s.push(e)}function n(e){var n=e.type==="readystatechange"&&i.readyState!=="complete";if(t.isReady||n)return;t.isReady=!0;for(var r=0,o=s.length;r<o;r++){var u=s[r];u()}}function r(r){return r.bind(i,"DOMContentLoaded",n),r.bind(i,"readystatechange",n),r.bind(e,"load",n),t}var i=e.document,s=[];t.isReady=!1,typeof define=="function"&&define.amd?(t.isReady=typeof requirejs=="function",define(["components/eventie/eventie"],r)):e.docReady=r(e.eventie)})(this);
+(function(e){function r(e){if(typeof e!="function")return;r.isReady?e():n.push(e)}function i(e){var i=e.type==="readystatechange"&&t.readyState!=="complete";if(r.isReady||i)return;r.isReady=!0;for(var s=0,o=n.length;s<o;s++){var u=n[s];u()}}function s(n){return n.bind(t,"DOMContentLoaded",i),n.bind(t,"readystatechange",i),n.bind(e,"load",i),r}var t=e.document,n=[];r.isReady=!1,typeof define=="function"&&define.amd?(r.isReady=typeof requirejs=="function",define(["components/eventie/eventie"],s)):e.docReady=s(e.eventie)})(this);
