7c12f50a9087994df08f9c20494ac1a1bfc5642c
Leo Chen <leo@saltedherring.com>
2016-04-13 23:14:58 +0100

    patch for SH live server (doesn't wanna take array('Slug'=>)

--- mainsite/code/Layout/WorkPage.php
+++ mainsite/code/Layout/WorkPage.php
@@ -125,7 +125,7 @@ class WorkPage_Controller extends Page_Controller {
 				}
 				
 				if ($this->request->param('Category')) {
-					if ($cat = DataObject::get_one('Category', array('Slug' => $this->request->param('Category')))) {
+					if ($cat = DataObject::get_one('Category', "Slug='" . $this->request->param('Category') . "'")) {
 						return Convert::raw2att($cat->MetaDescription);
 					}
 				}
@@ -156,7 +156,7 @@ class WorkPage_Controller extends Page_Controller {
 	
 	public function MetaTags($includeTitle = true) {
 		if ($cat = $this->request->param('Category')) {
-			$record = DataObject::get_one('Category', array('Slug' => $cat));
+			$record = DataObject::get_one('Category', "Slug='" . $cat ."'");
 		}else{
 			$record = $this;
 		}
