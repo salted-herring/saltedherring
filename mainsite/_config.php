0aa40022b019b763598745a634ea088268ec8e08
Simon Winter <simon@saltedherring.com>
2016-06-03 11:04:26 +1200

    SSL production test

--- mainsite/_config.php
+++ mainsite/_config.php
@@ -50,6 +50,6 @@ if (Director::isDev()) {
 }
 
 if (Director::isLive()) {
-	#Director::forceSSL();
+	Director::forceSSL();
 	SS_Log::add_writer(new SS_LogEmailWriter('administration@saltedherring.com'), SS_Log::ERR);
 }
